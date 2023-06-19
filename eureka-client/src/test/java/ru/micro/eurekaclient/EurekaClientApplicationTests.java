87429152b6d25da3656300d970d7c8d0cbf6ce2d
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-06-19 13:46:52 +0300

    Added: DockerFile and properties

--- eureka-client/src/test/java/ru/micro/eurekaclient/EurekaClientApplicationTests.java
+++ eureka-client/src/test/java/ru/micro/eurekaclient/EurekaClientApplicationTests.java
@@ -1,13 +1,1 @@
-package ru.micro.eurekaclient;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaClientApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
